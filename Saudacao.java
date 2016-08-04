bf1d62b4c47bde44fdc1584a1c8f861bf6ada093
Marcus Vinicius Veiga Roggero <marcus.roggero@b2wdigital.com>
2016-08-04 09:07:16 -0300

    Puxar nomes no arquivo

--- Saudacao.java
+++ Saudacao.java
@@ -1,7 +1,19 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 class Saudacao {
 
 	public static void main (String args[]) {
-		System.out.println(args[0]
-			+ " Bem-vindo ao curso");
+		
+		Scanner entrada = new Scanner(System.in);
+
+		// Aqui criamos um objeto que irá receber o nome do aluno
+		//System.out.print("Digite o nome do aluno: ");
+		//String nome = entrada.nextLine();
+
+		String nome = JOptionPane.showInputDialog("Digite o nome do fulano");
+
+		System.out.println("Bem-vindo ao curso "
+			+ nome);
 	}
 }
