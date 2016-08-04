7fc23e34a16343a57674eefb072bb16c2a9a804b
Marcus Vinicius Veiga Roggero <marcus.roggero@b2wdigital.com>
2016-08-04 09:39:16 -0300

    AreaTrianguloRetangulo

--- Saudacao.java
+++ Saudacao.java
@@ -5,15 +5,45 @@ class Saudacao {
 
 	public static void main (String args[]) {
 		
-		Scanner entrada = new Scanner(System.in);
+		//Scanner entrada = new Scanner(System.in);
 
 		// Aqui criamos um objeto que irá receber o nome do aluno
 		//System.out.print("Digite o nome do aluno: ");
 		//String nome = entrada.nextLine();
+		//String nome = JOptionPane.showInputDialog("Digite o nome do fulano");
 
-		String nome = JOptionPane.showInputDialog("Digite o nome do fulano");
+		
+		double base = Double.parseDouble(JOptionPane.showInputDialog("Base"));
+		double altura = Double.parseDouble(JOptionPane.showInputDialog("Altura"));
+		String tipoForma = JOptionPane.showInputDialog("Forma");
+		
+		System.out.println(tipoForma);
 
-		System.out.println("Bem-vindo ao curso "
-			+ nome);
+		if (tipoForma.equals("Triangulo")) {
+
+		System.out.println(areaTriangulo(base, altura));
+
+		} else {
+
+			System.out.println(areaRetangulo(base, altura));
+
+		}
+
 	}
+
+	public static double areaTriangulo(double a, double b) {
+
+			double area = (a * b)/2;
+			return area;
+
+	}
+
+	public static double areaRetangulo(double a, double b) {
+
+			double area = (a * b);
+			return area;
+
+	}
+
 }
+
